ac3be91e7a8cb3a975479e2f39c5f5d096a7fe83
Opeyemi Dada <dadaadeife@gmail.com>
2025-04-28 00:42:42 +0100

    Fixed

--- src/actions/api-key.ts
+++ src/actions/api-key.ts
@@ -1,54 +1,51 @@
-"use server";
-import { prisma } from "@/libs/prismaDb";
-import bcrypt from "bcrypt";
-import { revalidatePath } from "next/cache";
-import { isAuthorized } from "@/libs/isAuthorized";
-import { user } from "@/types"; // Import the User type
+// "use server";
+// import { prisma } from "@/libs/prismaDb";
+// import bcrypt from "bcrypt";
+// import { revalidatePath } from "next/cache";
+// import { isAuthorized } from "@/libs/isAuthorized";
 
-export async function getApiKeys() {
-  const user = await isAuthorized();
-  if (!user) {
-    return []; // If the user is not authorized, return an empty array
-  }
+// export async function getApiKeys() {
+//   const user = await isAuthorized();
+//   if (!user) {
+//     return null;
+//   }
+//   const res = await prisma.apiKey.findMany({
+//     where: {
+//       userId: user.id,
+//     },
+//   });
+//   return res;
+// }
 
-  const res = await prisma.apiKey.findMany({
-    where: {
-      userId: user.id, // Access the id property directly
-    },
-  });
-  return res;
-}
+// export async function createApiKey(keyName: string) {
+//   const user = await isAuthorized();
+//   if (!user) {
+//     return null;
+//   }
 
-export async function createApiKey(keyName: string) {
-  const user = await isAuthorized();
+//   const key = user.role as string;
 
-  if (!user) {
-    return null; // Return null if the user is not authorized
-  }
+//   // Hash the key
+//   const hashedKey = await bcrypt.hash(key, 10);
 
-  const key = user.role; // Use the user's role for the API key
+//   await prisma.apiKey.create({
+//     data: {
+//       name: keyName,
+//       key: hashedKey,
+//       userId: user.id,
+//     },
+//   });
 
-  // Hash the key
-  const hashedKey = await bcrypt.hash(key, 10);
+//   revalidatePath("/admin/api");
+// }
 
-  await prisma.apiKey.create({
-    data: {
-      name: keyName,
-      key: hashedKey,
-      userId: user.id, // Store the user's id
-    },
-  });
+// export async function deleteApiKey(id: string) {
+//   const res = await prisma.apiKey.delete({
+//     where: {
+//       id,
+//     },
+//   });
 
-  revalidatePath("/admin/api");
-}
-
-export async function deleteApiKey(id: string) {
-  const res = await prisma.apiKey.delete({
-    where: {
-      id,
-    },
-  });
-
-  revalidatePath("/admin/api");
-  return res;
-}
+//   revalidatePath("/admin/api");
+//   return res;
+// }
--- src/actions/types.ts
+++
@@ -1,8 +0,0 @@
-// types.ts
-export type user = {
-    id: string;
-    role: string;
-    name?: string | null;
-    email?: string | null;
-    image?: string | null;
-};
