785e40590a2f2dfc5888033cbbca4acd0c3fe2d6
Opeyemi Dada <dadaadeife@gmail.com>
2025-04-28 11:18:51 +0100

    Fixed Typescript Error

--- src/actions/api-key.ts
+++ src/actions/api-key.ts
@@ -1,51 +1,49 @@
-// "use server";
-// import { prisma } from "@/libs/prismaDb";
-// import bcrypt from "bcrypt";
-// import { revalidatePath } from "next/cache";
-// import { isAuthorized } from "@/libs/isAuthorized";
+"use server";
+import { prisma } from "@/libs/prismaDb";
+import bcrypt from "bcrypt";
+import { revalidatePath } from "next/cache";
+import { isAuthorized } from "@/libs/isAuthorized";
 
-// export async function getApiKeys() {
-//   const user = await isAuthorized();
-//   if (!user) {
-//     return null;
-//   }
-//   const res = await prisma.apiKey.findMany({
-//     where: {
-//       userId: user.id,
-//     },
-//   });
-//   return res;
-// }
+export async function getApiKeys() {
+  const user = await isAuthorized();
+  const res = await prisma.apiKey.findMany({
+    where: {
+      userId: user?.id as string,
+    },
+  });
+  return res;
+}
 
-// export async function createApiKey(keyName: string) {
-//   const user = await isAuthorized();
-//   if (!user) {
-//     return null;
-//   }
+export async function createApiKey(keyName: string) {
+  const user = await isAuthorized();
 
-//   const key = user.role as string;
+  if (!user) {
+    return null;
+  }
 
-//   // Hash the key
-//   const hashedKey = await bcrypt.hash(key, 10);
+  const key = user.role as string;
 
-//   await prisma.apiKey.create({
-//     data: {
-//       name: keyName,
-//       key: hashedKey,
-//       userId: user.id,
-//     },
-//   });
+  // Hash the key
+  const hashedKey = await bcrypt.hash(key, 10);
 
-//   revalidatePath("/admin/api");
-// }
+  await prisma.apiKey.create({
+    data: {
+      name: keyName,
+      key: hashedKey,
+      userId: user.id,
+    },
+  });
 
-// export async function deleteApiKey(id: string) {
-//   const res = await prisma.apiKey.delete({
-//     where: {
-//       id,
-//     },
-//   });
+  revalidatePath("/admin/api");
+}
 
-//   revalidatePath("/admin/api");
-//   return res;
-// }
+export async function deleteApiKey(id: string) {
+  const res = await prisma.apiKey.delete({
+    where: {
+      id,
+    },
+  });
+
+  revalidatePath("/admin/api");
+  return res;
+}
